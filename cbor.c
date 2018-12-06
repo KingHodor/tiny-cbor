98ee55feeaa3c8361003b32f601c36cc53ca6218
Ahmet Alptekin <ahmet@evercoin.com>
2018-12-05 23:41:35 -0800

    Update cbor.c

--- cbor.c
+++ cbor.c
@@ -2,12 +2,12 @@
 #include "cbor.h"
 #include "error_types.h"
 
+
 static void cbor_write_bytes(const u_int8_t *bytes, const size_t bytes_size, u_int8_t *new_bytes, size_t *written) {
     for (int i = 0; i < bytes_size; ++i) {
         new_bytes[(*written) + i] = bytes[i];
     }
     *written = *written + bytes_size;
-
 }
 
 static void cbor_write(u_int8_t value, u_int8_t *new_bytes, size_t *written) {
@@ -71,6 +71,10 @@ static void cbor_encode_bytes(u_int8_t *new_bytes, size_t *written, const u_int8
     cbor_write_bytes(bytes, bytes_size, new_bytes, written);
 }
 
+static void cbor_encode_raw(u_int8_t *new_bytes, size_t *written, const u_int8_t *bytes, size_t bytes_size){
+    cbor_write_bytes(bytes, bytes_size, new_bytes, written);
+}
+
 static void cbor_encode_map(u_int8_t *new_bytes, size_t *written, cborItem * parent){
     cbor_encode_type_and_length(MT_MAP, 0, new_bytes, written);
 }
@@ -79,6 +83,18 @@ static void cbor_encode_map(u_int8_t *new_bytes, size_t *written, cborItem * par
 static void cbor_encode_tag(u_int8_t *new_bytes, size_t *written, uint8_t *value, size_t value_size, long tag){
     cbor_encode_type_and_length(MT_TAG, tag, new_bytes, written);
     cbor_encode_bytes(new_bytes, written, value, value_size);
+}
+
+void cbor_encode_indefinite_length_array(u_int8_t *new_bytes, size_t *written, cborItem * parent){
+    uint8_t symbol_array = (MT_ARRAY << 5) + 31;
+    cbor_write_bytes(&symbol_array, 1, new_bytes, written);
+    cborItem *current = parent->firstItem;
+    while (current != NULL){
+        cbor_encode(new_bytes, written, current);
+        current = current->nextItem;
+    }
+    uint8_t symbol_primitive = (MT_PRIMITIVE << 5) + 31;
+    cbor_write_bytes(&symbol_primitive, 1, new_bytes, written);
 }
 
 int cbor_encode(u_int8_t *new_bytes, size_t *written, cborItem * item){
@@ -105,8 +121,14 @@ int cbor_encode(u_int8_t *new_bytes, size_t *written, cborItem * item){
         case MT_TAG  :
             cbor_encode_tag(new_bytes, written, item->bytes.bytes, item->bytes.size ,item->tag);
             break;
+        case MT_INDEFINITE  :
+            cbor_encode_indefinite_length_array(new_bytes, written, item);
+            break;
+        case MT_RAW :
+            cbor_encode_raw(new_bytes, written, item->bytes.bytes, item->bytes.size);
+            break;
         default :
-            return ENCOING_ERROR;
+            return CARDANO_ENCOING_ERROR;
     }
     return NO_ERROR;
 }
@@ -121,21 +143,39 @@ void cbor_create_integer(cborItem * item, long value){
     item->uint = value;
 }
 
+
 void cbor_create_tag(cborItem * item, uint8_t *value, size_t value_size, long tag){
     item->type = MT_TAG;
     item->tag = tag;
     memcpy(item->bytes.bytes, value, value_size);
-    item->bytes.size = (uint8_t) value_size;
+    item->bytes.size = (pb_size_t) value_size;
 }
+
 
 void cbor_create_bytes(cborItem * item, uint8_t *bytes, size_t byte_size){
     item->type = MT_BYTES;
+    memset(item->bytes.bytes, 0 , 128);
+    item->bytes.size = (pb_size_t) byte_size;
     memcpy(item->bytes.bytes, bytes, byte_size);
-    item->bytes.size = (uint8_t) byte_size;
+}
+
+
+void cbor_create_raw(cborItem * item, uint8_t *bytes, size_t byte_size){
+    item->type = MT_RAW;
+    memset(item->bytes.bytes, 0 , 128);
+    item->bytes.size = (pb_size_t) byte_size;
+    memcpy(item->bytes.bytes, bytes, byte_size);
 }
 
 void cbor_create_array(cborItem * parent){
     parent->type = MT_ARRAY;
+    parent->length = 0;
+    parent->nextItem = NULL;
+    parent->firstItem = NULL;
+}
+
+void cbor_infinite_create_array(cborItem * parent){
+    parent->type = MT_INDEFINITE;
     parent->length = 0;
     parent->nextItem = NULL;
     parent->firstItem = NULL;
